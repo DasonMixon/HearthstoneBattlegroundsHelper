e182cdec5a44f97b125516b8e75d8277f802f3a9
Mason Dixon <mason-dixon@teletracking.com>
2020-02-20 21:20:26 -0500

    Adding material ui and calling API for all heroes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Container, Grid, Paper } from '@material-ui/core'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      items: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&gameMode=battlegrounds&tier=hero&access_token=ACCESS_TOKEN_HERE')
+      .then(Response => Response.json())
+      .then(res => this.setState({items: res.cards}));
+  }
+
+  render() {
+    return (
+      <Container fixed className="App">
+        <Grid container spacing={5}>
+
+          {this.state.items.map((i, j) => 
+            <Grid item xs key={j}>
+              <Paper style={{textAlign: 'center'}}>
+                <h3>{i.name}</h3>
+                <img style={{display: 'block', margin: 'auto'}} src={i.battlegrounds.image} />
+              </Paper>
+            </Grid>)}
+        </Grid>
+        <ul>
+          
+        </ul>
+      </Container>
+    );
+  }
+
 }
 
 export default App;
