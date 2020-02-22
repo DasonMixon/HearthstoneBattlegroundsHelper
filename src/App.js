e67ddfb171d3ea637d10f406cee5ba262fd30c56
Mason Dixon <masondixon@Masons-MBP.fios-router.home>
2020-02-21 22:53:06 -0500

    Adding axios and handling when HS API is not available

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Container, Grid, Paper } from '@material-ui/core'
+import { Container, Grid, Paper, CircularProgress } from '@material-ui/core';
+import axios from 'axios';
 
 class App extends Component {
 
@@ -8,32 +9,55 @@ class App extends Component {
     super();
 
     this.state = {
+      isLoading: true,
+      apiConnection: {
+        unavailable: false,
+        error: null
+      },
       items: []
     }
   }
 
   componentDidMount() {
-    fetch('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&gameMode=battlegrounds&tier=hero&access_token=ACCESS_TOKEN_HERE')
-      .then(Response => Response.json())
-      .then(res => this.setState({items: res.cards}));
+    axios.get('https://us.api.blizzard.com/hearthstone/cards?locale=en_US&gameMode=battlegrounds&tier=hero&access_token=ACCESS_TOKEN_HERE')
+      .then(res => res.json())
+      .then(res => this.setState({items: res.cards, isLoading: false}))
+      .catch(error => {
+        console.log(error);
+        
+        this.setState({
+          apiConnection: {
+            unavailable: true,
+            error: error.message
+          },
+          isLoading: false
+        });
+    });
   }
 
   render() {
-    return (
-      <Container fixed className="App">
-        <Grid container spacing={5}>
 
-          {this.state.items.map((i, j) => 
-            <Grid item xs key={j}>
-              <Paper style={{textAlign: 'center'}}>
-                <h3>{i.name}</h3>
-                <img style={{display: 'block', margin: 'auto'}} src={i.battlegrounds.image} />
-              </Paper>
-            </Grid>)}
-        </Grid>
-        <ul>
-          
-        </ul>
+    let value;
+    if (this.state.isLoading) {
+      value = <CircularProgress />;
+    }
+    else if (this.state.apiConnection.unavailable) {
+      value = <h2>Unable to connect to the Hearthstone API server!</h2>;
+    } else {
+      value = <Grid container spacing={5}>
+        {this.state.items.map((i, j) => 
+          <Grid item xs key={j}>
+            <Paper style={{textAlign: 'center'}}>
+              <h3>{i.name}</h3>
+              <img style={{display: 'block', margin: 'auto'}} src={i.battlegrounds.image} />
+            </Paper>
+          </Grid>)}
+      </Grid>
+    }
+
+    return (
+      <Container fixed className="App" style={{textAlign: 'center'}}>
+        {value}
       </Container>
     );
   }
